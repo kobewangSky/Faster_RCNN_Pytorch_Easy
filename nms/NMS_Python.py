bfbab4211a2aa9435976e7162d9f4cefeb2730df
kobe <bluce54088@gmail.com>
2019-08-08 21:13:20 +0800

    edit

--- nms/NMS_Python.py
+++ nms/NMS_Python.py
@@ -4,34 +4,51 @@ import numpy as np
 
 class NMS(object):
     @staticmethod
-    def suppress(sorted_bboxes:Tensor, threshold:float):
-        keep_indices = []
+    def suppress(sorted_bboxes:Tensor, threshold:float)-> Tensor:
+
+        keep_indices = np.zeros(0)
 
         temp_sorted_bboxes = sorted_bboxes
 
-        while(len(temp_sorted_bboxes)>1):
 
-            AllArea = (temp_sorted_bboxes[:, 2] - temp_sorted_bboxes[:, 0]) * (
-                        temp_sorted_bboxes[:, 3] - temp_sorted_bboxes[:, 1])
+        while(len(temp_sorted_bboxes)):
 
-            keep_indices.append(temp_sorted_bboxes[0])
+            keep_indices = np.append(keep_indices, temp_sorted_bboxes[0].cpu().numpy())
+
+            AllArea = (temp_sorted_bboxes[0:, 2] - temp_sorted_bboxes[:, 0]) * (
+                        temp_sorted_bboxes[0:, 3] - temp_sorted_bboxes[:, 1])
+
+            # width1 = temp_sorted_bboxes[0][2] - temp_sorted_bboxes[0][0]
+            # heigh1 = temp_sorted_bboxes[0][3] - temp_sorted_bboxes[0][1]
+            # Area1 = width1 * heigh1
 
 
-            width1 = temp_sorted_bboxes[0][2] - temp_sorted_bboxes[0][0]
-            heigh1 = temp_sorted_bboxes[0][3] - temp_sorted_bboxes[0][1]
-            Area1 = width1 * heigh1
+            MaxX = Tensor.max(temp_sorted_bboxes[0][0], temp_sorted_bboxes[:, 0])
+            MaxY = Tensor.max(temp_sorted_bboxes[0][1], temp_sorted_bboxes[:, 1])
+
+            MinX = Tensor.min(temp_sorted_bboxes[0][2], temp_sorted_bboxes[:, 2])
+            MinY = Tensor.min(temp_sorted_bboxes[0][3], temp_sorted_bboxes[:, 3])
+
+            CoverArea = ((MinX - MaxX) * (MinY - MaxY))
+
+            #NotCoverAreaIndex = (CoverArea < 0).nonzero()
+
+            #CoverAreaIndex = (CoverArea > 0).nonzero()
+
+            IOUList = CoverArea / ((AllArea + AllArea[0]) - CoverArea)
+
+            NonCoverAndIousmallIndex = (IOUList < threshold).nonzero()
+
+            temp_sorted_bboxes = temp_sorted_bboxes[NonCoverAndIousmallIndex].reshape(-1, 4)
+
+        print('finish_ NMS')
+        return torch.from_numpy(keep_indices).float().cuda().reshape(-1, 4)
 
 
-            MaxX = Tensor.max(temp_sorted_bboxes[0][0], temp_sorted_bboxes[1:, 0])
-            MaxY = Tensor.max(temp_sorted_bboxes[0][1], temp_sorted_bboxes[1:, 1])
 
-            MinX = Tensor.min(temp_sorted_bboxes[0][2], temp_sorted_bboxes[1:, 2])
-            MinY = Tensor.min(temp_sorted_bboxes[0][3], temp_sorted_bboxes[1:, 3])
 
-            tempindexX = (MinX - MaxX) < 0
-            tempindexY = (MinY - MaxY) < 0
 
-            Notcoverindex = tempindexX | tempindexY
+
 
 
 
